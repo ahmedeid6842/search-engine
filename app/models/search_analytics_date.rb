53345e229ef1900d1ab0770e87dad9e64af463be
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-11 19:11:41 +0300

    fixed reference columns names

--- app/models/search_analytics_date.rb
+++ app/models/search_analytics_date.rb
@@ -1,4 +1,4 @@
 class SearchAnalyticsDate < ApplicationRecord
-  belongs_to :search_analytics
-  belongs_to :searcher, class_name: 'User'
+  belongs_to :search_analytic
+  belongs_to :users, class_name: 'User'
 end
--- db/migrate/20231011132457_create_search_analytics_dates.rb
+++ db/migrate/20231011132457_create_search_analytics_dates.rb
@@ -1,7 +1,7 @@
 class CreateSearchAnalyticsDates < ActiveRecord::Migration[7.0]
   def change
     create_table :search_analytics_dates do |t|
-      t.references :search_analytics, null: false, foreign_key: true
+      t.references :search_analytic, null: false, foreign_key: true
       t.references :users, null: false, foreign_key: true
       t.date :searched_date
       t.integer :hits, default: 0
