9d49235c4d65805a4cedb4e5617e1ac1648df423
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-11 16:50:26 +0300

    Implement the search controller to get the articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
     before_action :authenticate_user!
 
+    def search
+      query = params[:query]&.strip
+      return if query.blank?
+
+      @articles = Article.where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
+
+      render json: { articles: @articles }
+    end
+
     def index
       render
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index" 
 
+  post '/search', to: 'home#search'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
