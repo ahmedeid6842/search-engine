ffe073f42ef17ec21898176239002bafc12b17dd
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-11 21:37:22 +0300

    style the home and analtics view and get the last public search in last day

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,44 +3,52 @@ const searchInput = document.getElementById('search-input');
 const searchResultsContainer = document.getElementById('search-results');
 
 searchForm.addEventListener('submit', event => {
-  event.preventDefault();
-  updateSearchResults();
+    event.preventDefault();
+    updateSearchResults();
 });
 
 searchInput.addEventListener('input', updateSearchResults);
 
 function updateSearchResults() {
-  const query = searchInput.value.trim();
+    const query = searchInput.value.trim();
 
-  if (query.length === 0) {
-    searchResultsContainer.innerHTML = '';
-    return;
-  }
-  console.log(query);
-  const authenticityToken = document.querySelector('[name="csrf-token"]').content;
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': authenticityToken, 
-    },
-    body: JSON.stringify({ query }),
-  };
+    if (query.length === 0) {
+        searchResultsContainer.innerHTML = '';
+        return;
+    }
+    console.log(query);
+    const authenticityToken = document.querySelector('[name="csrf-token"]').content;
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': authenticityToken,
+        },
+        body: JSON.stringify({ query }),
+    };
 
-  fetch(`/search?query=${encodeURIComponent(query)}`, options)
-    .then(response => response.json())
-    .then(data => {
-      const articles = data.articles; 
-      const articlesHTML = articles.map(article => {
-        return `<div class="article">
-                  <h2>${article.title}</h2>
-                  <p>${article.content}</p>
+    fetch(`/search?query=${encodeURIComponent(query)}`, options)
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
+            let articlesHTML = '';
+            if (!articles.length) {
+                articlesHTML = `
+                    <p class="card-text">No articles found.</p>
+                `
+            } else {
+
+                articlesHTML = articles.map(article => {
+
+                    return `<div class="card-body">
+                  <h4 class="card-title">${article.title}</h4>
+                  <p class="card-text">${article.content}</p>
                 </div>`;
-      }).join('');
-
-      searchResultsContainer.innerHTML = articlesHTML;
-    })
-    .catch(error => {
-      console.error('Error occurred while fetching search results:', error);
-    });
+                }).join('');
+            }
+            searchResultsContainer.innerHTML = articlesHTML;
+        })
+        .catch(error => {
+            console.error('Error occurred while fetching search results:', error);
+        });
 }
--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,8 +4,10 @@ class AnalyticsController < ApplicationController
     def index
       user = current_user
   
+      global_last_day_search_analytics = SearchAnalytic.where(updated_at: 1.day.ago..DateTime.now).order(searched: :desc).limit(10)
+
       last_week_search_analytics = SearchAnalyticsDate.where(users: user, searched_date: 1.week.ago..Date.today).order(hits: :desc).limit(10)
   
-      render 'analytics', locals: { last_week_search_analytics: last_week_search_analytics }
+      render 'analytics', locals: { last_week_search_analytics: last_week_search_analytics, global_last_day_search_analytics: global_last_day_search_analytics }
     end
   end
