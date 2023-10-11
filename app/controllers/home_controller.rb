197917ff51047fcad4ba7aeae05847d3ca2f3eaa
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-11 20:23:06 +0300

    Added the global searchs in past 24 hours to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render
+    last_week_search_analytics = SearchAnalytic.where(updated_at: 1.day.ago..DateTime.now).order(searched: :desc).limit(10)
+    # Pass the last week's search analytics to the view.
+    @last_week_search_analytics = last_week_search_analytics
   end
 end
