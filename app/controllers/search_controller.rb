9f02bba976abf944d5e6ad6a0cb9add9ec55c2a9
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-12 04:24:51 +0300

    make update_search_analytics_dates controller as background job by sidekiq

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,24 +9,10 @@ class SearchController < ApplicationController
 
       if query.include?('?')
         UpdateSearchAnalyticsJob.perform_later({query: query, articles: @articles.to_a})
-        update_search_analytics_dates(query, current_user)
+        UpdateSearchAnalyticsDatesJob.perform_later({query: query, user: current_user})
       end
 
 
       render json: { articles: @articles }
-    end
-
-    def update_search_analytics_dates(query, user)
-      search_analytic = SearchAnalytic.find_by(query: query)
-    
-      search_analytic_date = SearchAnalyticsDate.find_or_initialize_by(
-        search_analytic: search_analytic, 
-        users: user,
-        searched_date: Date.today
-      )
-    
-      search_analytic_date.update(hits: search_analytic_date.hits + 1)
-
-      search_analytic_date.save
     end
 end
