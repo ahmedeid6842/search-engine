b075676e78e6fca4a55a1b9f2e830a29fb5fe5a6
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-12 15:29:36 +0300

    merge the update job into one job

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,8 +8,7 @@ class SearchController < ApplicationController
       @articles = Article.where('title ILIKE ? OR content ILIKE ?', "%#{query}%", "%#{query}%")
 
       if query.include?('?')
-        UpdateSearchAnalyticsJob.perform_later({query: query, articles: @articles.to_a})
-        UpdateSearchAnalyticsDatesJob.perform_later({query: query, user: current_user})
+        UpdateSearchAnalyticsJob.perform_later({query: query, articles: @articles.to_a, user: current_user})
       end
 
       render json: { articles: @articles.as_json(include: { author: { only: :name } }) }
--- app/jobs/update_search_analytics_job.rb
+++ app/jobs/update_search_analytics_job.rb
@@ -14,6 +14,16 @@ class UpdateSearchAnalyticsJob < ApplicationJob
       end
     end
 
+    search_analytic_date = SearchAnalyticsDate.find_or_initialize_by(
+      search_analytic: search_analytic, 
+      users: input[:user],
+      searched_date: Date.today
+    )
+  
+    search_analytic_date.update(hits: search_analytic_date.hits + 1)
+
+    
+    search_analytic_date.save
     search_analytic.save
   end
 end
