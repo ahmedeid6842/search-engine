f00b5e4aec9ce5778c7e6a968c8f371ba9c3a83f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-15 13:53:01 +0300

    implement methods to update day,week,month rank and reset hit

--- app/models/global_key_words_analytic.rb
+++ app/models/global_key_words_analytic.rb
@@ -1,2 +1,25 @@
 class GlobalKeyWordsAnalytic < ApplicationRecord
+    def self.update_previous_day_rank_reset
+        records = order(day_hit: :desc, updated_at: :desc)
+        records.each_with_index do |record, index|
+            record.update(previous_day_rank: index+1)
+        end
+        update_all(day_hit: 0)
+    end
+
+    def self.update_previous_week_rank_reset
+        records = order(week_hit: :desc, updated_at: :desc)
+        records.each_with_index do |record, index|
+            record.update(previous_week_rank: index+1)
+        end
+        update_all(week_hit: 0)
+    end
+
+    def self.update_previous_month_rank_reset
+        records = order(month_hit: :desc, updated_at: :desc)
+        records.each_with_index do |record, index|
+            record.update(previous_month_rank: index+1)
+        end
+        update_all(month_hit: 0)
+    end
 end
