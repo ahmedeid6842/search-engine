21d8374a0b5414e81339e37b7411ca43d175fb60
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-15 13:53:40 +0300

    use whenever schedule based on cron to execute keywords hit fuctions

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,14 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+env :PATH, ENV['PATH']
+set :output, './log/cron.log'
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+every 2.minutes do 
+    runner "GlobalKeyWordsAnalytic.update_previous_day_rank_reset"
+end
 
-# Learn more: http://github.com/javan/whenever
+every 1.week, at: '12pm' do 
+    runner "GlobalKeyWordsAnalytic.update_previous_week_rank_reset"
+end
+
+every 1.month, at: '12pm' do
+    runner "GlobalKeyWordsAnalytic.update_previous_month_rank_reset"
+end
