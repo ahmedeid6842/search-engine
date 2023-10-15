73b7500dbc0cf930aae964458109d202193c62ff
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-15 06:01:04 +0300

    Implement background job to update the keyword analtics

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,11 @@ class SearchController < ApplicationController
     def search
       query = params[:query]&.strip
       return if query.blank?
-
+      
       @articles = Article.where('title ILIKE ? OR content ILIKE ?', "%#{query}%", "%#{query}%")
-
+      
       if query.include?('?')
+        UpdateKeyWordsAnalyticsJob.perform_later({query: query, user:current_user})
         UpdateSearchAnalyticsJob.perform_later({query: query, articles: @articles.to_a, user: current_user})
       end
 
---
+++ app/jobs/update_key_words_analytics_job.rb
@@ -0,0 +1,7 @@
+class UpdateKeyWordsAnalyticsJob < ApplicationJob
+  queue_as :default
+
+  def perform(input)
+
+  end
+end
