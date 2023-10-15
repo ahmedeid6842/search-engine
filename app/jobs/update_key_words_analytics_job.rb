4f5a22e6afde1a222819e0a0a84f2494492b0090
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-15 06:03:06 +0300

    Implement update key words jobs to tokenizer the query

--- app/jobs/update_key_words_analytics_job.rb
+++ app/jobs/update_key_words_analytics_job.rb
@@ -2,6 +2,16 @@ class UpdateKeyWordsAnalyticsJob < ApplicationJob
   queue_as :default
 
   def perform(input)
-
+    user_keywords = UserKeyWordsAnalytic.find_or_initialize_by(users: input[:user]);
+    
+    keywords = query_tokenizer(input[:query]); 
   end
+
+  private 
+  def query_tokenizer(query)
+    tgr = EngTagger.new
+    tagged = tgr.add_tags(query)
+    tgr.get_nouns(tagged)
+  end
+
 end
